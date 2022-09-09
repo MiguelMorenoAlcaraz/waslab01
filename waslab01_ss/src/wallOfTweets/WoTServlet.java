e5ddce0b05a9948716f50709f45d93e6e097392c
alumne <alumne@pcrecanvib5>
2022-09-09 12:42:39 +0200

    tasca #2.2 (printPLAINresult)

--- waslab01_ss/src/wallOfTweets/WoTServlet.java
+++ waslab01_ss/src/wallOfTweets/WoTServlet.java
@@ -53,40 +53,52 @@ public class WoTServlet extends HttpServlet {
 
 		response.sendRedirect(request.getContextPath());
 	}
+	
+	private void printPLAINresult(Vector<Tweet> tweets) {
+		int count = 1; 
+		for (Tweet tweet: tweets) {
+			out.println("tweet #" count ": " tweet.getAuthor() ": " tweet.getText() " " tweet.getDate());
+			++count;
+		}
+	}
 
 	private void printHTMLresult (Vector<Tweet> tweets, HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
-		DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.FULL, currentLocale);
-		DateFormat timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
-		res.setContentType ("text/html");
-		res.setCharacterEncoding(ENCODING);
-		PrintWriter  out = res.getWriter ( );
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head><title>Wall of Tweets</title>");
-		out.println("<link href=\"wallstyle.css\" rel=\"stylesheet\" type=\"text/css\" />");
-		out.println("</head>");
-		out.println("<body class=\"wallbody\">");
-		out.println("<h1>Wall of Tweets</h1>");
-		out.println("<div class=\"walltweet\">"); 
-		out.println("<form method=\"post\">");
-		out.println("<table border=0 cellpadding=2>");
-		out.println("<tr><td>Your name:</td><td><input name=\"author\" type=\"text\" size=70></td><td></td></tr>");
-		out.println("<tr><td>Your tweet:</td><td><textarea name=\"tweet_text\" rows=\"2\" cols=\"70\" wrap></textarea></td>"); 
-		out.println("<td><input type=\"submit\" name=\"action\" value=\"Tweet!\"></td></tr>"); 
-		out.println("</table></form></div>");
-		String currentDate = "None";
-		for (Tweet tweet: tweets) {
-			String messDate = dateFormatter.format(tweet.getDate());
-			if (!currentDate.equals(messDate)) {
-				out.println("<br><h3>...... " + messDate + "</h3>");
-				currentDate = messDate;
+		if (req.getHeader(headereq).equals("text/plain")) printPLAINresult(tweets);
+		else
+		{
+			DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.FULL, currentLocale);
+			DateFormat timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
+			res.setContentType ("text/html");
+			res.setCharacterEncoding(ENCODING);
+			PrintWriter  out = res.getWriter ( );
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head><title>Wall of Tweets</title>");
+			out.println("<link href=\"wallstyle.css\" rel=\"stylesheet\" type=\"text/css\" />");
+			out.println("</head>");
+			out.println("<body class=\"wallbody\">");
+			out.println("<h1>Wall of Tweets</h1>");
+			out.println("<div class=\"walltweet\">"); 
+			out.println("<form method=\"post\">");
+			out.println("<table border=0 cellpadding=2>");
+			out.println("<tr><td>Your name:</td><td><input name=\"author\" type=\"text\" size=70></td><td></td></tr>");
+			out.println("<tr><td>Your tweet:</td><td><textarea name=\"tweet_text\" rows=\"2\" cols=\"70\" wrap></textarea></td>"); 
+			out.println("<td><input type=\"submit\" name=\"action\" value=\"Tweet!\"></td></tr>"); 
+			out.println("</table></form></div>");
+			String currentDate = "None";
+			for (Tweet tweet: tweets) {
+				String messDate = dateFormatter.format(tweet.getDate());
+				if (!currentDate.equals(messDate)) {
+					out.println("<br><h3>...... " + messDate + "</h3>");
+					currentDate = messDate;
+				}
+				out.println("<div class=\"wallitem\">");
+				out.println("<h4><em>" + tweet.getAuthor() + "</em> @ "+ timeFormatter.format(tweet.getDate()) +"</h4>");
+				out.println("<p>" + tweet.getText() + "</p>");
+				out.println("</div>");
 			}
-			out.println("<div class=\"wallitem\">");
-			out.println("<h4><em>" + tweet.getAuthor() + "</em> @ "+ timeFormatter.format(tweet.getDate()) +"</h4>");
-			out.println("<p>" + tweet.getText() + "</p>");
-			out.println("</div>");
-		}
-		out.println ( "</body></html>" );
+			out.println ( "</body></html>" );
+	}
 	}
 }
